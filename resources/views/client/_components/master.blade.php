ad8668cb176dfc991c333cbead5282bb53039ee1
Maulana Ichwan Anshory <64744726+masiwang@users.noreply.github.com>
2020-10-04 11:19:26 +0700

    fix client style.css

--- resources/views/client/_components/master.blade.php
+++ resources/views/client/_components/master.blade.php
@@ -8,6 +8,7 @@
       <link rel="shortcut icon" type="image/jpg" href="{{ asset('image/logo.ico')}}"/>
       <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;600;700&display=swap" rel="stylesheet"> 
       <title>@yield('title') - Makarya</title>
+      <link rel="stylesheet" href="/styles/client.css">
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
       <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
