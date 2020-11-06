bbbbeda7fa2e8c893f8b5d558b888eae56b74a9b
afifimaduddin <afif.imaduddin2104@gmail.com>
2020-11-06 21:49:48 +0700

    Update index.blade.php

--- resources/views/client/dashboard/profile/index.blade.php
+++ resources/views/client/dashboard/profile/index.blade.php
@@ -25,18 +25,18 @@
                         <li class="nav-item" role="presentation">
                           <a class="nav-link" id="transaction-tab" data-toggle="tab" href="#transaction" role="tab" aria-controls="transaction" aria-selected="false">Transaksi</a>
                         </li>
-                        <button type="button" class="btn btn-success btn-sm mb-2    ">
+                        <a type="button" class="btn btn-success btn-sm mb-2" href="/transaction/topup">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-credit-card" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
                                 <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
                               </svg> Topup
-                        </button>
-                        <button type="button" class="btn btn-primary btn-sm">
+                            </a>
+                        <a type="button" class="btn btn-primary btn-sm" href="/transaction/withdraw">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                 <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
                               </svg> Withdraw
-                        </button>
+                        </a>
                       </ul>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                 <div class="mb-4">
                     <div class="d-flex flex-row align-items-end">
                         <h2 class="mb-1 mr-4">@{{ user.name }}</h2> <p class="mb-2 text-secondary">@{{ user.job }} <span class="badge bg-primary">@{{ (user.gender === 1) ? 'L' : 'P' }}</span></p>
-                    </div>
+                    </div>  
                     <p class="text-info">
                         <span>
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
