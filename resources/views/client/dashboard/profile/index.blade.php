700b71967798e205c28004fef4d9e58ccaea84fc
afifimaduddin <afif.imaduddin2104@gmail.com>
2020-11-07 20:06:10 +0700

    Update index.blade.php

--- resources/views/client/dashboard/profile/index.blade.php
+++ resources/views/client/dashboard/profile/index.blade.php
@@ -102,6 +102,12 @@
                                 </tr>
                             </table>
                         </div>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                              </svg> edit profile
+                        </button>
                     </div>
                     <div class="tab-pane fade" id="transaction" role="tabpanel" aria-labelledby="transaction-tab" style="height: 400px; overflow-y: scroll">
                         <div class="p-3">
@@ -115,6 +121,69 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Profile</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action={{url('/profile')}} method="POST" class="row g-3">
+                        <div class="col-md-4">
+                            <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{$user->name}}">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="phone" class="form-label">Tanggal Lahir</label>
+                        <input id="datepicker" class="form-control" name="birthday" value="{{$user->birthday}}">
+                            @if (\Session::has('birthday'))
+                                <small class="text-danger">{{ \Session::get('birthday') }}</small>
+                            @endif
+                            <script>
+                                $("#datepicker").flatpickr();
+                            </script>
+                        </div>
+                        <div class="col-md-4">
+                            <p class="form-label">Jenis kelamin</p>
+                            <input type="hidden" name="gender">
+                            <select class="form-select"  id="gender" name="gender">
+                                @if ($user->gender==1)
+                                <option selected value="1">Laki-laki</option>
+                                <option value="2">Perempuan</option>    
+                                @else
+                                <option value="1">Laki-laki</option>
+                                <option selected value="2">Perempuan</option>    
+                                @endif
+                                
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                          <label for="inputAddress2" class="form-label">No KTP</label>
+                        <input type="text" class="form-control" id="inputAddress2" value="{{$user->ktp}}">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="job" class="form-label">Pekerjaan</label>
+                            <input type="text" class="form-control" id="email" name="email" value="{{$user->job}}">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="phone" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" value="{{$user->email}}">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="phone" class="form-label">Nomor HP</label>
+                            <input type="text" class="form-control" id="phone" name="phone" value="{{$user->phone}}">
+                        </div>
+                      </form>
+                </div>
+                <div class="modal-footer">
+                <a type="button" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+                <input type="submit" class="btn btn-primary" value="simpan">
+                </div>
+            </div>
             </div>
         </div>
         <script src="/script/profile-index.js"></script>
